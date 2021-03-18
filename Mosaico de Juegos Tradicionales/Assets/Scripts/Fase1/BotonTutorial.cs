2c6ed8430add77be97511a9d4d581099fd35e310
afdaza27 <af.daza@uniandes.edu.co>
2021-03-18 14:42:27 -0500

    botones saltar y seguir

--- Mosaico de Juegos Tradicionales/Assets/Scripts/Fase1/BotonTutorial.cs
+++ Mosaico de Juegos Tradicionales/Assets/Scripts/Fase1/BotonTutorial.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class BotonTutorial : MonoBehaviour
 {
     [SerializeField]
     float tiempo;
+
+    string ubicacion = "Misc/UI/";
     // Start is called before the first frame update
     void Start()
     {
+        Image imageComponent = gameObject.GetComponent<Image>();
         if (ConfigUtils.Fase == 1)
         {
             gameObject.SetActive(false);
             Invoke("Visibilizar", tiempo);
+            imageComponent.sprite = Resources.Load<Sprite>(ubicacion + "seguir");
+        }
+        else
+        {
+            imageComponent.sprite = Resources.Load<Sprite>(ubicacion + "saltar");
         }
         
     }
