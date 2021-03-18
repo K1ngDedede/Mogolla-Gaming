9fa628af7ad2e8cbbb5281d769382cdbbe77aeb0
Porygon Z and Eye of Truth <lj.garciam1@uniandes.edu.co>
2021-03-18 15:50:38 -0500

    lmao

---
+++ Mosaico de Juegos Tradicionales/Assets/Scripts/Utils/SplashText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashText : MonoBehaviour
+{
+
+    private int state;
+
+    private float scale;
+
+    [SerializeField]
+    public float sp;
+
+    [SerializeField]
+    public int iterator;
+
+    private int iteration;
+    void Start()
+    {
+        state = 1;
+        scale = 1;
+    }
+
+    
+    void Update()
+    {
+        scale += Time.deltaTime * sp * state;
+        iteration++;
+        if (iteration % iterator == 0) state*=-1;
+        gameObject.transform.localScale = new Vector3(scale,scale, 1);
+    }
+}
