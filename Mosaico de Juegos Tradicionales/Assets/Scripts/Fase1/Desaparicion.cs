40d2569152c89396a4ee64656382620f51bc863a
afdaza27 <af.daza@uniandes.edu.co>
2021-03-18 18:52:44 -0500

    Signos de interrogacion en cutscene fase 1

---
+++ Mosaico de Juegos Tradicionales/Assets/Scripts/Fase1/Desaparicion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desaparicion : MonoBehaviour
+{
+    [SerializeField]
+    float tiempo;
+    private void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Invisibilizar", tiempo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Invisibilizar()
+    {
+        gameObject.SetActive(false);
+    }
+}
