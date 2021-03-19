a3c4715570745e4203e3a1f1731b38d517ccf176
afdaza27 <af.daza@uniandes.edu.co>
2021-03-18 21:35:35 -0500

    efecto escritura

---
+++ Mosaico de Juegos Tradicionales/Assets/Scripts/Utils/Escritor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Escritor : MonoBehaviour
+{
+    Text txt;
+    string contenido;
+
+    [SerializeField]
+    float tiempoAparicion;
+
+    [SerializeField]
+    float duracionCaracter;
+
+    private void Awake()
+    {
+        txt = GetComponent<Text>();
+        contenido = txt.text;
+        txt.text = "";
+        Invoke("llamarEscribir", tiempoAparicion);
+    }
+
+    private void llamarEscribir()
+    {
+        StartCoroutine("Escribir");
+    }
+
+    IEnumerator Escribir()
+    {
+        foreach(char c in contenido)
+        {
+            txt.text += c;
+            yield return new WaitForSeconds(duracionCaracter);
+        }
+    }
+}
