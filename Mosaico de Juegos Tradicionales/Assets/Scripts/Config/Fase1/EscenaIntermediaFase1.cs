29d332a17358b2d2f18d5b94c00eddfdd09cc685
afdaza27 <af.daza@uniandes.edu.co>
2021-03-18 20:59:44 -0500

    animaciones barra de paciencia

--- Mosaico de Juegos Tradicionales/Assets/Scripts/Config/Fase1/EscenaIntermediaFase1.cs
+++ Mosaico de Juegos Tradicionales/Assets/Scripts/Config/Fase1/EscenaIntermediaFase1.cs
@@ -28,6 +28,7 @@ public class EscenaIntermediaFase1 : MonoBehaviour
         //Barra medidora de paciencia
         GameObject barraPaciencia = GameObject.FindGameObjectWithTag("barraPaciencia");
         SpriteRenderer rendererBarra = barraPaciencia.GetComponent<SpriteRenderer>();
+        Animation animation = barraPaciencia.GetComponent<Animation>();
         
 
         //Apariencia abuela
@@ -40,34 +41,38 @@ public class EscenaIntermediaFase1 : MonoBehaviour
         {
             case 4:
                 spriteAbuela = Resources.Load<Sprite>(ubicacionAbuela + "AbuelitaFeliz");
-                rendererBarra.color = alto;
-                posBarra = new Vector3(0, 2.761f, 0);
-                escalaBarra = new Vector3(1.395553f, 0.2241068f, 0);
+                //rendererBarra.color = alto;
+                //posBarra = new Vector3(0, 2.761f, 0);
+                //escalaBarra = new Vector3(1.395553f, 0.2241068f, 0);
+                
                 break;
             case 3:
                 spriteAbuela = Resources.Load<Sprite>(ubicacionAbuela + "AbuelitaSeria");
-                rendererBarra.color = medio;
-                posBarra = new Vector3(-0.919f, 2.761f, 0);
-                escalaBarra = new Vector3(1.036599f, 0.2241068f, 0);
+                //rendererBarra.color = medio;
+                //posBarra = new Vector3(-0.919f, 2.761f, 0);
+                //escalaBarra = new Vector3(1.036599f, 0.2241068f, 0);
+                animation.Play("3vidas");
                 break;
             case 2:
                 spriteAbuela = Resources.Load<Sprite>(ubicacionAbuela + "AbuelitaSeria");
-                rendererBarra.color = medio;
-                posBarra = new Vector3(-1.775f, 2.761f, 0);
-                escalaBarra = new Vector3(0.7022681f, 0.2241068f, 0);
+                //rendererBarra.color = medio;
+                //posBarra = new Vector3(-1.775f, 2.761f, 0);
+                //escalaBarra = new Vector3(0.7022681f, 0.2241068f, 0);
+                animation.Play("2vidas");
                 break;
             default:
                 spriteAbuela = Resources.Load<Sprite>(ubicacionAbuela + "AbuelitaSeria");
-                rendererBarra.color = bajo;
-                posBarra = new Vector3(-2.667f, 2.761f, 0);
-                escalaBarra = new Vector3(0.3537103f, 0.2241068f, 0);
+                //rendererBarra.color = bajo;
+                //posBarra = new Vector3(-2.667f, 2.761f, 0);
+                //escalaBarra = new Vector3(0.3537103f, 0.2241068f, 0);
+                animation.Play("1vida");
                 break;
 
         }
        
         spriteRendererAbuela.sprite = spriteAbuela;
-        barraPaciencia.transform.position = posBarra;
-        barraPaciencia.transform.localScale = escalaBarra;
+        //barraPaciencia.transform.position = posBarra;
+        //barraPaciencia.transform.localScale = escalaBarra;
 
         timerCambioEscena = Camera.main.gameObject.AddComponent<Timer>();
         timerCambioEscena.Duration = 5;
