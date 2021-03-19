d0cca3f0e57e63206d1a00a43e1eefb5b972135d
unknown <lm.rodriguezc2@uniandes.edu.co>
2021-03-19 13:38:59 -0500

    Cambios fuchi

---
+++ Mosaico de Juegos Tradicionales/Assets/Scripts/Microjuegos/Fuchi/GameControlScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControlScript : MonoBehaviour
+{
+	
+	public GameObject objectToDisable;
+	public GameObject objectToDisable2;
+	
+	public static bool disabled = true;
+	public static bool disabled2 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(!disabled2){
+			objectToDisable2.SetActive(true);
+
+		}
+		else{
+			objectToDisable2.SetActive(false);
+
+		}
+        if(disabled){
+			objectToDisable.SetActive(false);
+
+		}
+		else{
+			objectToDisable.SetActive(true);
+
+		}
+
+    }
+}
---
+++ Mosaico de Juegos Tradicionales/Assets/Scripts/Microjuegos/Fuchi/Relojito.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Relojito : MonoBehaviour
+{
+    
+	public static float currentTime = FuchiUtils.currentTime;
+	float startingTime = FuchiUtils.startingTime;
+	[SerializeField] Text RelojitoTimer;
+    static Text textoTiempo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        currentTime = startingTime;
+    }
+	
+	void Update()
+	{
+		currentTime -= 1*Time.deltaTime;
+		RelojitoTimer.text = (currentTime.ToString("0")+ "s");
+		if(currentTime <= 0)
+		{
+			currentTime = 0;
+		}
+	}
+
+}
