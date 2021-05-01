a9b6fa1b1efb74b12f59395b1d62e52eb04e509e
afdaza27 <af.daza@uniandes.edu.co>
2021-05-01 00:49:13 -0500

    fondo rana

---
+++ Mosaico de Juegos Tradicionales/Assets/Scripts/Microjuegos/Rana/BGRana.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGRana : MonoBehaviour
+{
+    private Material bgm;
+
+    private Texture2D vic;
+
+    private Texture2D def;
+
+    private bool state;
+
+    private float offX;
+
+    private float offY;
+
+    private float sX;
+
+    private float sY;
+    void Start()
+    {
+        bgm = gameObject.GetComponent<Renderer>().material;
+        vic = (Texture2D)Resources.Load("Microjuegos/Rana/Sprites/f2");
+        def = (Texture2D)Resources.Load("Microjuegos/Trompo/Sprites/ff");
+        bgm.mainTexture = vic;
+        bgm.mainTextureScale = new Vector2(5f, 5f);
+        vic.wrapMode = TextureWrapMode.Repeat;
+        def.wrapMode = TextureWrapMode.Repeat;
+        state = true;
+        offX = 0;
+        offY = 0;
+        sX = 0.1f;
+        sY = 0.15f;
+    }
+
+    void Update()
+    {
+        if (state)
+        {
+            offX += Time.deltaTime * sX;
+            offY += Time.deltaTime * sY;
+            bgm.mainTextureOffset = new Vector2(offX, offY);
+        }
+    }
+
+    public void halt()
+    {
+        state = !state;
+        bgm.mainTexture = def;
+    }
+
+
+}
