0debd86fc172c187a64ecf431a6033a95dd86e18
afdaza27 <af.daza@uniandes.edu.co>
2021-05-24 21:55:39 -0500

    constancia en volumen

---
+++ Mosaico de Juegos Tradicionales/Assets/Scripts/Utils/Volumen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Volumen : MonoBehaviour
+{
+    public AudioMixer mixer;
+    private void Start()
+    {
+        float volumen = PlayerPrefs.GetFloat("VolumenMusica", 0.75f);
+        mixer.SetFloat("VolMusica", Mathf.Log10(volumen) * 20);
+    }
+
+}
