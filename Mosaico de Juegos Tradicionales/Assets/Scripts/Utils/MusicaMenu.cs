3c45105fcef9761935128f75d7761f541dabf70f
afdaza27 <af.daza@uniandes.edu.co>
2021-05-25 21:43:43 -0500

    Musica incluida

---
+++ Mosaico de Juegos Tradicionales/Assets/Scripts/Utils/MusicaMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicaMenu : MonoBehaviour
+{
+    string ubicacionPrefabs = "Utils/Prefabs/";
+    GameObject musica;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.FindGameObjectWithTag("musicaMenu") == null)
+        {
+            musica = Resources.Load<GameObject>(ubicacionPrefabs + "MusicaMenu");
+            Instantiate(musica);
+            DontDestroyOnLoad(GameObject.FindGameObjectWithTag("musicaMenu"));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DestruirMusicaMenu()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("musicaMenu"));
+    }
+}
